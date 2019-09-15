96a2ce45612b9726403ed069f1de28e8f081e8bb
Ruhan Khandakar <khandakarruhan@gmail.com>
2019-09-15 08:22:19 +0530

    broken our home into smaller comonents
    finished styling our menu item component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 import "./App.css";
-import HomePage from "./components/HomePage/HomePage";
+
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
